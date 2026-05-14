e09715d04b096cb0a9cd63225fb58bfcf019bf67
Ryan Kraay <6840071+ryan-kraay@users.noreply.github.com>
2026-05-14 20:09:51 +0200

    chore: unset experimental_approve_all_reads

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,4 @@ import pytest
 @pytest.fixture(autouse=True)
 def disable_color(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setenv("COLOR", "disabled")
+    monkeypatch.setenv("EXPERIMENTAL_APPROVE_ALL_READS", "")
